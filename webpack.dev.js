b5c9859841ecf8b97a28e0474bd36b40d5cb22ba
Patrick Ojeh <patrickojeh@gmail.com>
2022-01-08 11:30:35 +0100

    configure sass loaders for webpack

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,6 +4,14 @@ const HTMLWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: './src/client/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  },
   plugins: [
     new HTMLWebpackPlugin({
       template: './src/client/views/index.html',
