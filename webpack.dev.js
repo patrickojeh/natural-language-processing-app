fea98e64cdc07790194f3d126065c14d2688b497
Patrick Ojeh <patrickojeh@gmail.com>
2022-01-08 11:12:49 +0100

    add html-webpack-plugin to webpack dev config

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,12 @@
 const webpack = require('webpack');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/client/index.js',
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './src/client/views/index.html',
+      filename: 'index.html'
+    })
+  ]
 }
