33b580462a2a6d8bf6b8b5469d0e3e121ebf404d
Patrick Ojeh <patrickojeh@gmail.com>
2022-01-10 15:03:25 +0100

    modified form handler function

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -6,7 +6,28 @@ export function handleSubmit(event) {
 
   // validate url
   if (Client.validateUrl(url)) {
+    event.target.value = 'Please wait';
 
+    const formData = {url}
+
+    fetch('http://localhost:8080/api', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData)
+    })
+    .then(res => {
+      return res.json();
+    })
+    .then(res => {
+      const {agreement, confidence, irony, model, subjectivity} = res;
+      const resultContainer = document.getElementById('result');
+      console.log(res)
+      // const paragraph = document.createElement('p');
+      // paragraph.innerHTML = `Agreement: ${agreement} <br/> Confidence: ${confidence} <br/> Irony: ${irony} <br/> Model: ${model} <br/> Subjectivity: ${subjectivity}`
+      // formResultsContainer.appendChild(paragraph);
+    });
   } else {
     // TODO: show error message.
   }
