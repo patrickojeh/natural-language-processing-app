43ed2639f918a9a9980223233e74df6a5138e851
Patrick Ojeh <patrickojeh@gmail.com>
2022-01-12 11:52:37 +0100

    increase font size

--- src/client/js/urlValidator.js
+++ src/client/js/urlValidator.js
@@ -1,3 +1,3 @@
 export function validateUrl(url) {
-  return /^(http[s]?:\/\/)?(www\.)?[\w\d_\-\/\?]+\.(\w)+([\w\d_\-\/\?])+?$/i.test(url);
+  return /^(http[s]?:\/\/)?(\w+\.)?[\w\d_\-\/\?]+\.(\w)+([\w\d_\-\/\?])+?$/i.test(url);
 }
