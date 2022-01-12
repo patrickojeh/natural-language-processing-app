5926e04d044b65768114aca5aff82a70eed06e2b
Patrick Ojeh <patrickojeh@gmail.com>
2022-01-12 11:55:52 +0100

    add webpack prod config

---
+++ webpack.prod.js
@@ -0,0 +1,30 @@
+const webpack = require('webpack');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/client/index.js',
+  output: {
+    libraryTarget: 'var',
+    library: 'Client'
+  },
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        test: /\.js$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './src/client/views/index.html',
+      filename: 'index.html'
+    })
+  ]
+}
