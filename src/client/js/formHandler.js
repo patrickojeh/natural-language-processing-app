0d02d147064baa80a5c28e348d57a9cc58e4eed8
Patrick Ojeh <patrickojeh@gmail.com>
2022-01-13 15:18:21 +0100

    change url to relative path

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -10,7 +10,7 @@ export function handleSubmit(event) {
 
     const formData = {url}
 
-    fetch('http://localhost:8080/api', {
+    fetch('/api', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
